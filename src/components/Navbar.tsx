6e4947fa94ad6b3269c159d4161ea196434d754a
zman811 <sunburnzman@gmail.com>
2022-08-31 09:23:51 -0600

    Changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,12 @@
 export default function Navbar() {
   return (
-    <div>
-      <a href="/test">test</a>
-    </div>
+    <ul style={{ listStyleType: "none", margin: 0, padding: 0, display: 'inline' }}>
+      <li>
+        <a href="/test">test</a>
+      </li>
+      <li>
+        <a href="/testing">testing</a>
+      </li>
+    </ul>
   );
 }
